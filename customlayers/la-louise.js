6df222abf162346244dfba13b953e64d7e121dcf
cballevre <celestin.ballevre@protonmail.com>
2023-11-18 21:54:33 +0100

    feat: Add a line

--- customlayers/la-louise.js
+++ customlayers/la-louise.js
@@ -8,25 +8,54 @@ const styles = {
   Point: new ol.style.Style({
     image: image,
   }),
+  LineString: new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: "green",
+      width: 10,
+    }),
+  }),
 };
 
 const styleFunction = function (feature) {
-  console.log(feature.getGeometry().getType());
   return styles[feature.getGeometry().getType()];
 };
 
-const geoJson = new ol.source.Vector({
-  format: new ol.format.GeoJSON(),
-  url: "data/2021-10-la-louise-clean.geojson",
+const layer = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    format: new ol.format.GeoJSON(),
+    url: "data/2021-08-northabout-clean.geojson",
+  }),
 });
 
-console.log("geoJson", geoJson);
+const source = layer.getSource();
+source.on("featuresloadend", function () {
+  const features = source.getFeatures();
+  const points = features.map((feature) => {
+    const geometry = feature.getGeometry();
+    const coordinates = geometry.getCoordinates();
+    const properties = feature.getProperties();
 
-const layer = new ol.layer.Vector({
-  source: geoJson,
+    return { coordinates, datetime: properties.Datetime };
+  });
+
+  const orderPoints = points.sort(function (a, b) {
+    return new Date(b.datetime) - new Date(a.datetime);
+  });
+
+  const test = orderPoints.map((newObject) => {
+    return newObject.coordinates;
+  });
+
+  const line = new ol.Feature({
+    geometry: new ol.geom.LineString(test),
+  });
+
+  source.addFeature(line);
+});
+
+const newLayer = new ol.layer.Vector({
+  source: source,
   style: styleFunction,
 });
 
-console.log("layer", layer);
-
-new CustomLayer("la-louise", layer);
+new CustomLayer("la-louise", newLayer);
